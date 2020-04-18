d9f3e54d8222a4dc49ebda447f45af97e3ce9ae7
Kanna <hamster.happiness+gitkraken@gmail.com>
2020-04-19 01:27:45 +0900

    プロジェクト ファイルを追加します。

---
+++ TamiradiOtayoriReader/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace TamiradiOtayoriReader.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
